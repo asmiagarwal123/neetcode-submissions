749919f9592f0aa4f7cb304b660cb165de1c1650
Asmi Agarwal <asmi.agarwal2006@gmail.com>
2026-09-06 12:40:22 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // Put every useful number in its correct position
+        for (int i = 0; i < n; i++) {
+
+            while (nums[i] >= 1 && nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        // Find the first number that is not in its correct position
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
